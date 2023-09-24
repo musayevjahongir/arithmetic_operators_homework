e8f876f499cda396b99fedb1de53721a9090a014
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-24 18:19:08 +0500

    kod yozdim

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,9 +1,9 @@
 #Create a variable called 'number' and assign any value of int to it.
 number=25
 #Divide the number by 2 and assign number.
-number=number%2
+number=number/2
 #Find the fraction of the number and assign to a variable called 'answer'.
-answer=number/2
+answer=number%1
 #Print the answer.
 print(answer)
 #Print the number.
